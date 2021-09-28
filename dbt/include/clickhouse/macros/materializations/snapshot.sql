82183652c22251572094d6f7418483089345a6b6
Evgeny Odegov <evg.odegov@gmail.com>
2021-09-28 17:12:56 +0700

    Fix snapshot.sql

--- dbt/include/clickhouse/macros/materializations/snapshot.sql
+++ dbt/include/clickhouse/macros/materializations/snapshot.sql
@@ -107,6 +107,92 @@
 
 {% endmaterialization %}
 
+{% macro snapshot_staging_table(strategy, source_sql, target_relation) -%}
+  with snapshot_query as (
+      {{ source_sql }}
+  ),
+  snapshotted_data as (
+    select *,
+      {{ strategy.unique_key }} as dbt_unique_key
+    from {{ target_relation }}
+    where dbt_valid_to is null
+  ),
+  insertions_source_data as (
+    select
+      *,
+      {{ strategy.unique_key }} as dbt_unique_key,
+      {{ strategy.updated_at }} as dbt_updated_at,
+      {{ strategy.updated_at }} as dbt_valid_from,
+      nullif({{ strategy.updated_at }}, {{ strategy.updated_at }}) as dbt_valid_to,
+      {{ strategy.scd_id }} as dbt_scd_id
+    from snapshot_query
+  ),
+  updates_source_data as (
+    select
+        *,
+        {{ strategy.unique_key }} as dbt_unique_key,
+        {{ strategy.updated_at }} as dbt_updated_at,
+        {{ strategy.updated_at }} as dbt_valid_from,
+        {{ strategy.updated_at }} as dbt_valid_to
+    from snapshot_query
+  ),
+  {%- if strategy.invalidate_hard_deletes %}
+  deletes_source_data as (
+    select
+      {{ strategy.unique_key }} as dbt_unique_key
+    from snapshot_query
+  ),
+  {% endif %}
+  insertions as (
+    select
+      'insert' as dbt_change_type,
+      source_data.*
+    from insertions_source_data as source_data
+    left outer join snapshotted_data on snapshotted_data.dbt_unique_key = source_data.dbt_unique_key
+    where snapshotted_data.dbt_unique_key is null
+      or (
+        snapshotted_data.dbt_unique_key is not null
+      and (
+        {{ strategy.row_changed }}
+      )
+    )
+  ),
+  updates as (
+    select
+      'update' as dbt_change_type,
+      source_data.*,
+      snapshotted_data.dbt_scd_id
+    from updates_source_data as source_data
+    join snapshotted_data on snapshotted_data.dbt_unique_key = source_data.dbt_unique_key
+    where (
+      {{ strategy.row_changed }}
+    )
+  )
+  {%- if strategy.invalidate_hard_deletes -%}
+  ,
+  deletes as (
+    select
+      'delete' as dbt_change_type,
+      source_data.*,
+      {{ snapshot_get_time() }} as dbt_valid_from,
+      {{ snapshot_get_time() }} as dbt_updated_at,
+      {{ snapshot_get_time() }} as dbt_valid_to,
+      snapshotted_data.dbt_scd_id
+    from snapshotted_data
+    left join deletes_source_data as source_data on snapshotted_data.dbt_unique_key = source_data.dbt_unique_key
+    where source_data.dbt_unique_key is null
+  )
+  {%- endif %}
+
+  select * from insertions
+  union all
+  select * from updates
+  {%- if strategy.invalidate_hard_deletes %}
+  union all
+  select * from deletes
+  {%- endif %}
+  SETTINGS join_use_nulls=1
+{%- endmacro %}
 
 {% macro clickhouse__snapshot_merge_sql_one(target, source, insert_cols, upsert) -%}
   {%- set insert_cols_csv = insert_cols | join(', ') -%}
@@ -128,7 +214,7 @@
 
   {% call statement('insert_updated_and_deleted') %}
     insert into {{ upsert }} ({{ insert_cols_csv }})
-    with updates as (
+    with updated_or_deleted as (
       select
         dbt_scd_id,
         dbt_valid_to
@@ -139,7 +225,7 @@
       {%- if column != 'dbt_valid_to' -%}
         target.{{ column }} as {{ column }}
       {%- else -%}
-        updates.dbt_valid_to as dbt_valid_to
+        updated_or_deleted.dbt_valid_to as dbt_valid_to
       {%- endif %} {%- if not loop.last %}, {%- endif %}
     {%- endfor %}
     from {{ target }} target
